a099b64af8d00004525deb4edc2b2300f5b3ae1e
Newt Industries <info@newtindustries.com>
2020-01-06 15:10:50 -0600

    created models and properties for customer and employee

---
+++ TrashCollection/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollection.Models
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State{ get; set; }
+        public string Zip { get; set; }
+        public double Balance { get; set; }
+        public string SuspendStart { get; set; }
+        public string SuspendEnd { get; set; }
+
+    }
+}
